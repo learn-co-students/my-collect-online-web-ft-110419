a7be668f44cf05f998843d56545e435765770a32
Sean Good <tree662@protonmail.com>
2019-11-12 03:39:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+  i = 0
+  empty_collection = []
+  while i < collection.length 
+    empty_collection << yield(collection[i])
+    i += 1 
+  end
+  return empty_collection
+end
